067cb8b2297cfc27389dca0b3c87d7cafdf0ef88
Dante <jamesmidzi@gmail.com>
2021-09-10 22:09:29 +0200

    rendered first header item title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
     <heade className="">
-      <h2 className="text-3xl">I am header</h2>
+      <div>
+        <HeaderItem title="HOME" />
+      </div>
       <Image
         className="object-contain"
         src="/images/hulu-logo.png"
---
+++ src/components/Header/HeaderItem.js
@@ -0,0 +1,9 @@
+function HeaderItem({ title }) {
+  return (
+    <div>
+      <p>{title}</p>
+    </div>
+  );
+}
+
+export default HeaderItem;
