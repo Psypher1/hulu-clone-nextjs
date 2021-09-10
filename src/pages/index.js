ebddcd748c1ae7eabba633ddc6d4606f37cfdb81
Dante <jamesmidzi@gmail.com>
2021-09-10 21:56:12 +0200

    created components folder and header file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -10,11 +11,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-3xl uppercase text-center text-gray-800 mt-8">
+      <h1 className="text-2xl uppercase text-center text-blue-600 mt-8">
         We Are Above The System
       </h1>
 
       {/* Header */}
+      <Header />
 
       {/* Nav */}
 
