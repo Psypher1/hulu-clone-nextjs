7e530b176f3ae79836181f94e5e7ff618bb701f6
Dante <jamesmidzi@gmail.com>
2021-09-11 20:26:28 +0200

    added icons to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,24 @@
-import { HomeIcon } from "@heroicons/react/outline";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
-    <heade className="">
-      <div>
+    <header className="">
+      <div className="flex flex-grow justify-evenly">
         <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image
         className="object-contain"
@@ -14,7 +26,7 @@ function Header() {
         width={200}
         height={100}
       />
-    </heade>
+    </header>
   );
 }
 
