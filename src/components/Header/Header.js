5248b1789087dc98a448aec7aefea993b3aeb8aa
Dante <jamesmidzi@gmail.com>
2021-09-11 20:48:49 +0200

    header sort of done - I am not happy with some spacing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,8 +11,8 @@ import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
-    <header className="">
-      <div className="flex flex-grow justify-evenly">
+    <header className="flex flex-col sm:flex-row m-4  justify-between items-center h-auto">
+      <div className="flex flex-grow  justify-evenly max-w-xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
         <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
@@ -20,12 +20,14 @@ function Header() {
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
         <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
-      <Image
-        className="object-contain"
-        src="/images/hulu-logo.png"
-        width={200}
-        height={100}
-      />
+      <div>
+        <Image
+          className="object-contain m-0 p-0"
+          src="/images/hulu-logo.png"
+          width={150}
+          height={100}
+        />
+      </div>
     </header>
   );
 }
